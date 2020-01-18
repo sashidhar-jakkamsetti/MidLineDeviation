efdfdec25362df5d863e716060a69ade47cf70c0
Sashidhar Jakkamsetti <sashidhar.jakkamsetti@gmail.com>
2020-01-18 12:45:20 -0800

    real physical distance update and some smoothing

---
+++ constants.py
@@ -0,0 +1,8 @@
+SCALE_AVG_NFRAMES = 50
+REAL_SQAURE_LEN = 21.5
+OPENING_DELTA_CHECK = 1.2
+DEVIATION_DELTA_CHECK = 0.6
+
+cascade_path='detector_architectures/haarcascade_frontalface_default.xml'
+PREDICTOR_PATH = "detector_architectures/shape_predictor_68_face_landmarks.dat"
+output_figures_path = 'figures/'
--- mid_line_analysis.py
+++ mid_line_analysis.py
@@ -1,13 +1,32 @@
 import cv2
 import dlib
 import numpy
+import math
+import sys
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
+import constants
 
-PREDICTOR_PATH = "detector_architectures/shape_predictor_68_face_landmarks.dat"
-predictor = dlib.shape_predictor(PREDICTOR_PATH)
-cascade_path='detector_architectures/haarcascade_frontalface_default.xml'
-cascade = cv2.CascadeClassifier(cascade_path)
+predictor = dlib.shape_predictor(constants.PREDICTOR_PATH)
+cascade = cv2.CascadeClassifier(constants.cascade_path)
+
+def measure_scale(vid_file):
+    vidcap = cv2.VideoCapture(vid_file)
+    success, image = vidcap.read()
+    scale = 0
+    count = 0
+    while success and count < constants.SCALE_AVG_NFRAMES:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        flags = 0
+        flags |= cv2.CALIB_CB_ADAPTIVE_THRESH
+        flags |= cv2.CALIB_CB_FAST_CHECK
+        flags |= cv2.CALIB_CB_NORMALIZE_IMAGE
+        ret, corners = cv2.findChessboardCorners(gray, (9,7), flags)
+        if ret == True:
+            dist = math.hypot(corners[1][0, 0] - corners[0][0, 0], corners[1][0, 1] - corners[0][0, 1])
+            scale += constants.REAL_SQAURE_LEN / dist
+            count += 1
+        success, image = vidcap.read()
+    return scale / constants.SCALE_AVG_NFRAMES
 
 def get_landmarks(im):
     rects = cascade.detectMultiScale(im, scaleFactor=1.3, minNeighbors=3, minSize=(50,50))
@@ -15,19 +34,36 @@ def get_landmarks(im):
     rect=dlib.rectangle(x,y,x+w,y+h)
     return numpy.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
 
-vidcap = cv2.VideoCapture('videos/JawDeviation.mp4')
+vid_file = sys.argv[1]
+scale = measure_scale(vid_file)
+
+vidcap = cv2.VideoCapture(vid_file)
 success,image = vidcap.read()
 count = 0
-hor_dist = [0]
-ver_dist = [0]
+hor_dist = []
+ver_dist = []
+prev_verd = 0
+prev_hord = 0
+first = True
 while success:
     try:
         landmarks = get_landmarks(image)
-        verd = landmarks[30][0, 1] - landmarks[8][0, 1]
-        hord = landmarks[30][0, 0] - landmarks[8][0, 0]
-        ver_dist.append(verd)
-        hor_dist.append(hord)
-        print("frame: {}, opening: {}, deviation: {}".format(count, verd, hord))
+        verd = (landmarks[8][0, 1] - landmarks[30][0, 1])*scale
+        hord = (landmarks[8][0, 0] - landmarks[30][0, 0])*scale
+        if first:
+            first = False
+            ver_dist.append(verd)
+            hor_dist.append(hord)
+        else:
+            if abs(prev_verd - verd) > constants.OPENING_DELTA_CHECK or abs(prev_hord - hord) > constants.DEVIATION_DELTA_CHECK:
+                ver_dist.append(ver_dist[-1])
+                hor_dist.append(hor_dist[-1])
+            else:
+                ver_dist.append(verd)
+                hor_dist.append(hord)
+        prev_hord = hord
+        prev_verd = verd
+        print("frame: {}, opening: {}, deviation: {} \t PROCESSED -> opening: {}, deviation: {}".format(count, verd, hord, ver_dist[-1], hor_dist[-1]))
     except:
         print('frame: {} failed to identify'.format(count))
         ver_dist.append(ver_dist[-1])
@@ -35,11 +71,16 @@ while success:
     success,image = vidcap.read()    
     count += 1
 
-# x = numpy.array([i for i in range(len(hor_dist))])
-# y = numpy.array(hor_dist)
-# f = interp1d(x, y, kind='quadratic')
-# y_smooth = f(x)
-plt.plot(hor_dist, 'C2', label='deviation', markevery=10)
-#plt.plot(ver_dist, 'C1', label='opening')
+ver_dist = [x - ver_dist[0] for x in ver_dist]
+
+plt.figure(0)
+plt.plot(hor_dist, 'C2', label='deviation (in mm)')
+plt.plot(ver_dist, 'C1', label='opening (in mm)')
 plt.legend()
+plt.savefig("{}opening-deviation.{}.png".format(constants.output_figures_path, vid_file.split("/")[1]))
+
+plt.figure(1)
+plt.plot(hor_dist, 'C2', label='deviation (in mm)')
+plt.legend()
+plt.savefig("{}deviation.{}.png".format(constants.output_figures_path, vid_file.split("/")[1]))
 plt.show()
