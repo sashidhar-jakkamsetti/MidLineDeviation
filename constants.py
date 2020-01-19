f49610465f8f7ce951b1f7b4d210a8b29cf43668
Sashidhar Jakkamsetti <sashidhar.jakkamsetti@gmail.com>
2020-01-19 13:33:44 -0800

    measurements done

--- constants.py
+++ constants.py
@@ -3,6 +3,10 @@ REAL_SQAURE_LEN = 21.5
 OPENING_DELTA_CHECK = 1.2
 DEVIATION_DELTA_CHECK = 0.6
 
+verbose = True
+
 cascade_path='detector_architectures/haarcascade_frontalface_default.xml'
-PREDICTOR_PATH = "detector_architectures/shape_predictor_68_face_landmarks.dat"
-output_figures_path = 'figures/'
+predictor_path = "detector_architectures/shape_predictor_68_face_landmarks.dat"
+output_figures_path = 'output/figures/'
+output_log_path = 'output/logs/'
+output_measurements_path = 'output/measurements/'
--- mid_line_analysis.py
+++ mid_line_analysis.py
@@ -3,10 +3,11 @@ import dlib
 import numpy
 import math
 import sys
+import os
 import matplotlib.pyplot as plt
 import constants
 
-predictor = dlib.shape_predictor(constants.PREDICTOR_PATH)
+predictor = dlib.shape_predictor(constants.predictor_path)
 cascade = cv2.CascadeClassifier(constants.cascade_path)
 
 def measure_scale(vid_file):
@@ -30,57 +31,90 @@ def measure_scale(vid_file):
 
 def get_landmarks(im):
     rects = cascade.detectMultiScale(im, scaleFactor=1.3, minNeighbors=3, minSize=(50,50))
-    x,y,w,h =rects[0]
+    x,y,w,h = rects[0]
     rect=dlib.rectangle(x,y,x+w,y+h)
-    return numpy.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
+    return numpy.matrix([[p.x, p.y] for p in predictor(im, rect).parts()]), rects[0]
 
-vid_file = sys.argv[1]
-scale = measure_scale(vid_file)
+def get_measurements(landmarks, facebox, scale, out_file):
+    ver1 = (landmarks[36][0, 0] - facebox[0])*scale
+    ver2 = (landmarks[39][0, 0] - landmarks[36][0, 0])*scale
+    ver3 = (landmarks[42][0, 0] - landmarks[39][0, 0])*scale
+    ver4 = (landmarks[45][0, 0] - landmarks[42][0, 0])*scale
+    ver5 = (facebox[0] + facebox[2] - landmarks[45][0, 0])*scale
+    hor1 = (int((landmarks[19][0, 1] + landmarks[24][0, 1])/2) - facebox[1])*scale
+    hor2 = (landmarks[33][0, 1] - int((landmarks[19][0, 1] + landmarks[24][0, 1])/2))*scale
+    hor3 = (int((landmarks[50][0, 1] + landmarks[52][0, 1])/2) - landmarks[33][0, 1])*scale
+    hor4 = (facebox[1] + facebox[3] - int((landmarks[50][0, 1] + landmarks[52][0, 1])/2))*scale
+    print_str = "vertical segments: {} \t {} \t {} \t {} \t {}\nhorizontal segments: {} \t {} \t {} \t {}"\
+                    .format(ver1, ver2, ver3, ver4, ver5, hor1, hor2, hor3, hor4)
+    out_file.write(print_str + "\n")
+    if constants.verbose: print(print_str)
 
-vidcap = cv2.VideoCapture(vid_file)
-success,image = vidcap.read()
-count = 0
-hor_dist = []
-ver_dist = []
-prev_verd = 0
-prev_hord = 0
-first = True
-while success:
-    try:
-        landmarks = get_landmarks(image)
-        verd = (landmarks[8][0, 1] - landmarks[30][0, 1])*scale
-        hord = (landmarks[8][0, 0] - landmarks[30][0, 0])*scale
-        if first:
-            first = False
-            ver_dist.append(verd)
-            hor_dist.append(hord)
-        else:
-            if abs(prev_verd - verd) > constants.OPENING_DELTA_CHECK or abs(prev_hord - hord) > constants.DEVIATION_DELTA_CHECK:
-                ver_dist.append(ver_dist[-1])
-                hor_dist.append(hor_dist[-1])
-            else:
+
+def run(filename):
+    scale = measure_scale(filename)
+    vidcap = cv2.VideoCapture(filename)
+    success,image = vidcap.read()
+    count = 0
+    hor_dist = []
+    ver_dist = []
+    prev_verd = 0
+    prev_hord = 0
+    first = True
+    vid_file_name = filename.replace('\\','/').split('/')[-1]
+    out_logfile = open("{}{}.log.txt".format(constants.output_log_path, vid_file_name), "w")
+    out_measurefile = open("{}{}.measurements.txt".format(constants.output_measurements_path, vid_file_name), "w")
+
+    while success:
+        try:
+            landmarks, facebox = get_landmarks(image)
+            verd = (landmarks[8][0, 1] - landmarks[30][0, 1])*scale
+            hord = (landmarks[8][0, 0] - landmarks[30][0, 0])*scale
+            if first:
+                first = False
                 ver_dist.append(verd)
                 hor_dist.append(hord)
-        prev_hord = hord
-        prev_verd = verd
-        print("frame: {}, opening: {}, deviation: {} \t PROCESSED -> opening: {}, deviation: {}".format(count, verd, hord, ver_dist[-1], hor_dist[-1]))
-    except:
-        print('frame: {} failed to identify'.format(count))
-        ver_dist.append(ver_dist[-1])
-        hor_dist.append(hor_dist[-1])
-    success,image = vidcap.read()    
-    count += 1
+                get_measurements(landmarks, facebox, scale, out_measurefile)
+            else:
+                if abs(prev_verd - verd) > constants.OPENING_DELTA_CHECK or abs(prev_hord - hord) > constants.DEVIATION_DELTA_CHECK:
+                    ver_dist.append(ver_dist[-1])
+                    hor_dist.append(hor_dist[-1])
+                else:
+                    ver_dist.append(verd)
+                    hor_dist.append(hord)
+            prev_hord = hord
+            prev_verd = verd
+            print_str = "frame: {}, opening: {}, deviation: {} \t PROCESSED -> opening: {}, deviation: {}".format(count, verd, hord, ver_dist[-1], hor_dist[-1])
+            out_logfile.write(print_str + "\n")
+            if constants.verbose: print(print_str)
+        except:
+            print_str = "frame: {} failed to identify".format(count)
+            out_logfile.write(print_str + "\n")
+            if constants.verbose: print(print_str)
+            ver_dist.append(ver_dist[-1])
+            hor_dist.append(hor_dist[-1])
+        success,image = vidcap.read()    
+        count += 1
 
-ver_dist = [x - ver_dist[0] for x in ver_dist]
+    out_logfile.close()
+    out_measurefile.close()
+    ver_dist = [x - ver_dist[0] for x in ver_dist]
+    plt.figure()
+    plt.plot(hor_dist, 'C2', label='deviation (in mm)')
+    plt.plot(ver_dist, 'C1', label='opening (in mm)')
+    plt.legend()
+    plt.savefig("{}opening-deviation.{}.png".format(constants.output_figures_path, vid_file_name))
+    plt.figure()
+    plt.plot(hor_dist, 'C2', label='deviation (in mm)')
+    plt.legend()
+    plt.savefig("{}deviation.{}.png".format(constants.output_figures_path, vid_file_name))
 
-plt.figure(0)
-plt.plot(hor_dist, 'C2', label='deviation (in mm)')
-plt.plot(ver_dist, 'C1', label='opening (in mm)')
-plt.legend()
-plt.savefig("{}opening-deviation.{}.png".format(constants.output_figures_path, vid_file.split("/")[1]))
 
-plt.figure(1)
-plt.plot(hor_dist, 'C2', label='deviation (in mm)')
-plt.legend()
-plt.savefig("{}deviation.{}.png".format(constants.output_figures_path, vid_file.split("/")[1]))
-plt.show()
+vid_file = sys.argv[1]
+if vid_file.endswith('/') or vid_file.endswith('\\'):
+    for filename in os.listdir(vid_file):
+        print(vid_file + filename)
+        run(vid_file + filename)
+else:
+    run(vid_file)
+    plt.show()
