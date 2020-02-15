a0d92ce29dee5419fa716ad589a5abfdf4dc23d5
Sashidhar Jakkamsetti <sashidhar.jakkamsetti@gmail.com>
2020-02-15 11:31:44 -0800

    demo photos facial landmarks

--- detect_face_landmarks.py
+++ detect_face_landmarks.py
@@ -1,30 +1,33 @@
 import cv2
 import dlib
 import numpy
+import constants
+import sys
 
-PREDICTOR_PATH = "detector_architectures/shape_predictor_68_face_landmarks.dat"
-predictor = dlib.shape_predictor(PREDICTOR_PATH)
-cascade_path='detector_architectures/haarcascade_frontalface_default.xml'
-cascade = cv2.CascadeClassifier(cascade_path)
+predictor = dlib.shape_predictor(constants.predictor_path)
+cascade = cv2.CascadeClassifier(constants.cascade_path)
 
-def get_landmarks(im):
+def annotate_landmarks(im):
+    im = im.copy()
     rects = cascade.detectMultiScale(im, 1.3,5)
     x,y,w,h =rects[0]
     rect=dlib.rectangle(x,y,x+w,y+h)
-    return numpy.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
-
-def annotate_landmarks(im, landmarks):
-    im = im.copy()
+    landmarks = numpy.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
     for idx, point in enumerate(landmarks):
         pos = (point[0, 0], point[0, 1])
         cv2.putText(im, str(idx), pos,
                     fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-                    fontScale=0.4,
-                    color=(0, 0, 255))
-        cv2.circle(im, pos, 3, color=(0, 255, 255))
+                    fontScale=1,
+                    color=(255, 0, 0),
+                    thickness=2)
+        cv2.circle(im, pos, 3, color=(0, 0, 0), thickness=3)
+        cv2.rectangle(im,(x,y),(x+w,y+h),(0, 0, 255),5) 
     return im
 
-im=cv2.imread('images/sample.jpg')
-cv2.imshow('Result',annotate_landmarks(im,get_landmarks(im)))
+infn = sys.argv[1]
+im = cv2.imread(infn)
+flim = annotate_landmarks(im)
+#cv2.imshow('Result',flim)
+cv2.imwrite(infn.replace(".JPG", "") + ".faciallandmarks.jpg", flim)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
